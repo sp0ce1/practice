21ceae9fca154a55ff6a67df14fbd724548d92cd
Jonny <jonik1207blask@gmail.com>
2026-04-26 20:39:02 +0900

    feat: learn python array

---
+++ array.set.py
@@ -0,0 +1,27 @@
+'''
+Array & set
+(1) Array
+(2) Set
+(3) Specific operator with set
+'''
+
+# print("======= Array ========= ")
+
+from array import array
+
+# numbers = array('i', [1, 4, 5, 7, 8, 41])
+# print("numbers1:", numbers)
+
+# numbers.append(100)
+# numbers.insert(0, 14)
+# print("numbers2:", numbers)
+
+# numbers.remove(5)
+# numbers.pop()
+# print("numbers3:", numbers)
+
+# del numbers[0:2]
+# print("numbers4:", numbers)
+
+
+print("======= Set ========= ")
